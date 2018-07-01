237515ebd83299b35364b363adc015f440b8870b
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-07-01 12:59:53 +0100

    logo on home with onPress

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Button} from 'react-native'
+import { View, Button, Image, StyleSheet, TouchableHighlight } from 'react-native';
 
 
 
@@ -7,15 +7,35 @@ export default class Home extends React.Component {
 
   render() {
     return (
-     
+
       <View>
-          <Button
-          title= 'CityList'
-          onPress={() => this.props.navigation.navigate('CityList')}
+        <TouchableHighlight style={styles.logoContainer} onPress={() => this.props.navigation.navigate('CityList')} >
+          <Image style={styles.logo} source={require('../public/logo.png')} />
+        </TouchableHighlight>
+
+
+
+        {/* <Button
+            title='CityList'
+            onPress={() => this.props.navigation.navigate('CityList')}
           >
-          </Button>
-        </View>
-    
+          </Button> */}
+      </View >
+
     )
   }
 }
+
+const styles = StyleSheet.create({
+  logoContainer: {
+
+  },
+  logo: {
+
+    marginTop: 50,
+    alignSelf: 'stretch',
+    width: 350,
+    height: 300
+  }
+
+})

908a56590ef6ebaa560aa17828e858fa353342f9
vitorleo80 <vitorleo@hotmail.com>
2018-07-01 14:21:45 +0100

    image fixed

--- components/CityHeader.js
+++ components/CityHeader.js
@@ -12,13 +12,10 @@ export default class View3 extends Component {
     }
     return (
 
-      
-      <ImageBackground style={styles.city} source={{ uri: header.avatar_url }} >
-
-        <Text style={styles.text}>{header.name}</Text>
-
-      </ImageBackground>
-
+      <View>
+      <Image style={styles.city} source={{ uri: header.avatar_url }} />
+        <Text style={styles.text}>London</Text>
+      </View>
     );
   }
 }
@@ -28,10 +25,26 @@ const styles = StyleSheet.create({
 
   city: {
 
-    borderBottomWidth: 3,
-    borderBottomColor: 'white',
+    // borderBottomWidth: 3,
+    borderColor: 'white',
     marginTop: 5,
-    height: 370
+    height: 100,
+    borderWidth: 5
+  },
+  text: {
+    position: 'absolute', 
+    top: 40, 
+    left: 140, 
+    right: 0, 
+    bottom: 0, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    color: 'white',
+    fontSize: 20,
+    backgroundColor: 'rgba(0,0,0,0)',
+    fontWeight: 'bold'
   }
 
 })
+
+  // < Text style = { styles.text } > { header.name }</Text>

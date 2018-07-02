2850b03dc2141a834a5d5eece1f5d14a445a8aa9
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-07-02 10:56:36 +0100

    homepage redone

--- components/Home.js
+++ components/Home.js
@@ -1,21 +1,48 @@
 import React from 'react'
-import {View, Button} from 'react-native'
+import { View, Button, Image, StyleSheet, TouchableHighlight, Text } from 'react-native';
+import { Font } from 'expo'
+import Zocial from '../node_modules/@expo/vector-icons/fonts/Zocial.ttf'
 
-
+Font.loadAsync('Zocial', '../node_modules/@expo/vector-icons/fonts/Zocial.ttf')
 
 export default class Home extends React.Component {
 
   render() {
     return (
-     
+
       <View>
-          <Button
-          title= 'CityList'
-          onPress={() => this.props.navigation.navigate('CityList')}
-          >
-          </Button>
-        </View>
-    
+        <TouchableHighlight style={styles.logoContainer} onPress={() => this.props.navigation.navigate('CityList')} >
+          <Image style={styles.logo} source={require('../public/logo.png')} />
+        </TouchableHighlight>
+        <Text style={styles.text}>Tripster</Text>
+        {/* <Image source={require('../public/skyline.png')} style={styles.skyline} /> */}
+      </View >
+
     )
   }
 }
+
+const styles = StyleSheet.create({
+  logoContainer: {
+
+  },
+  logo: {
+    marginTop: 0,
+    alignSelf: 'stretch',
+    width: 350,
+    height: 300
+  },
+  text: {
+    alignSelf: 'center',
+    color: '#29a8c8',
+    fontFamily: 'Noteworthy',
+    fontSize: 50
+  },
+  skyline: {
+    alignSelf: 'center',
+    marginBottom: 0,
+    height: 300,
+    width: 370
+  }
+
+})
